51efdc6a035c87b7e3149951b56b2bda42a8f191
Arnav Borborah <arnavborborah11@gmail.com>
2018-03-20 12:39:54 +0000

    Added Doxygen generated documentation

--- include/project-abbr/factorial.hpp
+++ include/project-abbr/factorial.hpp
@@ -1,7 +1,55 @@
+/**
+ * @file
+ * @author Arnav Borborah <arnavborborah11@gmail.com>
+ * 
+ * @section License
+ * 
+ * factorial.hpp - Contains declaration of function factorial
+ * 
+ * Copyright (C) 2018  Arnav Borborah
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @section Description
+ * 
+ * Contains the declaration of the function factorial
+ * 
+ */
+
 #pragma once
 #ifndef FACTORIAL_HPP_
 #define FACTORIAL_HPP_
 
+/**
+ * 
+ * \brief Function that returns the factorial of an integer
+ * 
+ * The factorial function returns the product of all 
+ * positive integers less or equal to an integer number. 
+ * If number is less than 0, then the function returns 1.
+ * 
+ * \param number The integer to get the factorial of
+ * 
+ * \return The factorial of number
+ * 
+ * \section Example
+ * \code
+ * int fact10 = factorial(10); // fact10 contains 3628800
+ * int factNeg10 = factorial(-10); // factNeg10 contains 1
+ * \endcode
+ * 
+ */
 int factorial(int number);
 
 #endif // !FACTORIAL_HPP_
--- include/project-abbr/hello_world.hpp
+++ include/project-abbr/hello_world.hpp
@@ -1,21 +1,96 @@
+/**
+ * @file
+ * @author Arnav Borborah <arnavborborah11@gmail.com>
+ * 
+ * @section License
+ * 
+ * HelloWorld.hpp - Contains declaration of class HelloWorld
+ * 
+ * Copyright (C) 2018  Arnav Borborah
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @section Description
+ * 
+ * Contains the declaration of the HelloWorld class.
+ * 
+ */
+
 #pragma once
 #ifndef HELLO_WORLD_HPP_
 #define HELLO_WORLD_HPP_
 
 #include <string>
 
+/**
+ * 
+ * A useful class that contains utilities 
+ * helpful to "Hello, World!" programs.
+ * 
+ * \section Example
+ * \code
+ * HelloWorld helloWorld;
+ * 
+ * std::string helloWorld = helloWorld.hello() + ", " + helloWorld.world() + "!"; // helloWorld contains "Hello, World!"
+ * int randomNumber = helloWorld.generateRandomNumber(); // randomNumber contains 4000
+ * \endcode
+ * 
+ */
 class HelloWorld
 {
 public:
+    /**
+     * 
+     * \brief Constructor for HelloWorld class 
+     * 
+     * Initializes class members hello_ and world_
+     * with their respective values.
+     * 
+     */
     HelloWorld();
     
+    /**
+     * 
+     * \brief Getter for member variable hello_ 
+     * 
+     * \return The string "Hello"
+     * 
+     */
     std::string hello() const;
+    
+    /**
+     * 
+     * \brief Getter for member variable world_ 
+     * 
+     * \return The string "World"
+     * 
+     */
     std::string world() const;
     
+    /**
+     * 
+     * \brief A random number generator
+     * 
+     * \return The random number 4000
+     * 
+     */
     int generateRandomNumber() const;
+    
 private:
     std::string hello_;
     std::string world_;
+    
 };
 
 #endif // !HELLO_WORLD_HPP_
